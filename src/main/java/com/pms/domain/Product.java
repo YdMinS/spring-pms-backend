c5a44d49c46fa4778b03bb9e544fa3c139f51e6e
ydmins <dr.ydmins@gmail.com>
2026-04-15 18:26:20 +0900

    feat: connect db

---
+++ src/main/java/com/pms/domain/Product.java
@@ -0,0 +1,31 @@
+package com.pms.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
